3df552d0f281cc3016ea15042d8233e47cd176fe
Matheus Pacheco <matheusavpacheco@gmail.com>
2019-12-19 21:03:42 +0900

    Worked on creation dialog appearance. Just missing dropshadow

--- app/src/main/java/com/matpacheco/flashcards/MainActivity.java
+++ app/src/main/java/com/matpacheco/flashcards/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -79,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
                 mBuilder.setView(mView);
                 AlertDialog dialog = mBuilder.create();
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
         });
