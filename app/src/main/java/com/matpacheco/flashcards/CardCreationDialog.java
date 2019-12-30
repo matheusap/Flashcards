e9f3b1eac0a9d6dae19a654412af0e42dd468fd5
Matheus Pacheco <matheusavpacheco@gmail.com>
2019-12-30 18:40:25 +0900

    Worked more on card creation dialog, need to fix save button and work on functionality

---
+++ app/src/main/java/com/matpacheco/flashcards/CardCreationDialog.java
@@ -0,0 +1,67 @@
+package com.matpacheco.flashcards;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+public class CardCreationDialog extends DialogFragment
+{
+    public static final String TAG = "example_dialog";
+
+    private Toolbar toolbar;
+
+    public static CardCreationDialog display(FragmentManager fragmentManager) {
+        CardCreationDialog exampleDialog = new CardCreationDialog();
+        exampleDialog.show(fragmentManager, TAG);
+        return exampleDialog;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.AppTheme_FullScreenDialog);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+            dialog.getWindow().setWindowAnimations(R.style.AppTheme_Slide);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.card_creation_dialog, container, false);
+
+        toolbar = view.findViewById(R.id.toolbar);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        toolbar.setNavigationOnClickListener(v -> dismiss());
+//        toolbar.setTitle("Create a Flashcard");
+//        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setBackgroundColor(Color.WHITE);
+        toolbar.inflateMenu(R.menu.card_creation_dialog);
+        toolbar.setOnMenuItemClickListener(item -> {
+            dismiss();
+            return true;
+        });
+    }
+}
--- app/src/main/java/com/matpacheco/flashcards/MainActivity.java
+++ app/src/main/java/com/matpacheco/flashcards/MainActivity.java
@@ -43,54 +43,61 @@ public class MainActivity extends AppCompatActivity {
         deckButton2.setPadding(50,50,10,50);
         Button deckButton3 = (Button)getLayoutInflater().inflate(R.layout.deckbutton,null);
         deckButton3.setPadding(50,50,10,50);
+//        deckButton3.setElevation(500);
 
 
         layout.addView(deckButton);
         layout.addView(deckButton2);
         layout.addView(deckButton3);
         FloatingActionButton mCreateCard = (FloatingActionButton) findViewById(R.id.fabCreateCard);
-        mCreateCard.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
-                View mView = getLayoutInflater().inflate(R.layout.dialog_create, null);
-                final EditText mSideA = (EditText)mView.findViewById(R.id.side_a);
-                final EditText mSideB = (EditText)mView.findViewById(R.id.side_b);
-                Button mCreate = (Button)mView.findViewById(R.id.create);
-
-                mCreate.setOnClickListener(new View.OnClickListener()
-                {
-                    @Override
-                    public void onClick(View v)
-                    {
-                        if(!mSideA.getText().toString().isEmpty() && !mSideB.getText().toString().isEmpty())
-                        {
-                            Toast.makeText(MainActivity.this,
-                                    R.string.success_creation_msg,
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                        else
-                        {
-                            Toast.makeText(MainActivity.this,
-                                    R.string.failed_creation_message,
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-                mBuilder.setView(mView);
-                AlertDialog dialog = mBuilder.create();
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
-            }
-        });
+        mCreateCard.setOnClickListener(v -> openDialog());
+//                new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view)
+//            {
+//                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+//                View mView = getLayoutInflater().inflate(R.layout.dialog_create, null);
+//                final EditText mSideA = (EditText)mView.findViewById(R.id.side_a);
+//                final EditText mSideB = (EditText)mView.findViewById(R.id.side_b);
+//                Button mCreate = (Button)mView.findViewById(R.id.create);
+//
+//                mCreate.setOnClickListener(new View.OnClickListener()
+//                {
+//                    @Override
+//                    public void onClick(View v)
+//                    {
+//                        if(!mSideA.getText().toString().isEmpty() && !mSideB.getText().toString().isEmpty())
+//                        {
+//                            Toast.makeText(MainActivity.this,
+//                                    R.string.success_creation_msg,
+//                                    Toast.LENGTH_SHORT).show();
+//                        }
+//                        else
+//                        {
+//                            Toast.makeText(MainActivity.this,
+//                                    R.string.failed_creation_message,
+//                                    Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//
+//                mBuilder.setView(mView);
+//                AlertDialog dialog = mBuilder.create();
+//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                dialog.show();
+//            }
+//        });
 
 //        populateCardsLists();
 
 //        TextView reviews_available = (TextView)findViewById(R.id.reviews_available);
 //        reviews_available.setText("" + reviewable.size());
 
+    }
+
+    private void openDialog()
+    {
+        CardCreationDialog.display(getSupportFragmentManager());
     }
 
     private void populateCardsLists()
